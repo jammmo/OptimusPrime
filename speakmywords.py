24c7f6277ddfc95aa572e4f7fd8fc910b837f703
Jamie Moschella <40072741+jammmo@users.noreply.github.com>
2019-01-19 16:15:46 -0500

    Add files via upload

--- speakmywords.py
+++ speakmywords.py
@@ -6,35 +6,39 @@ Note: ssml must be well-formed according to:
 from google.cloud import texttospeech
 import pygame
 
-# Instantiates a client
-client = texttospeech.TextToSpeechClient()
+def main(synthtext):
+    # Instantiates a client
+    client = texttospeech.TextToSpeechClient()
 
-# Set the text input to be synthesized
-synthesis_input = texttospeech.types.SynthesisInput(text="supercalifragilisticexpialidocious")
+    # Set the text input to be synthesized
+    synthesis_input = texttospeech.types.SynthesisInput(text=synthtext)
 
-# Build the voice request, select the language code ("en-US") and the ssml
-# voice gender ("neutral")
-voice = texttospeech.types.VoiceSelectionParams(
-    language_code='en-US',
-    ssml_gender=texttospeech.enums.SsmlVoiceGender.NEUTRAL)
+    # Build the voice request, select the language code ("en-US") and the ssml
+    # voice gender ("neutral")
+    voice = texttospeech.types.VoiceSelectionParams(
+        language_code='en-US',
+        ssml_gender=texttospeech.enums.SsmlVoiceGender.NEUTRAL)
 
-# Select the type of audio file you want returned
-audio_config = texttospeech.types.AudioConfig(
-    audio_encoding=texttospeech.enums.AudioEncoding.MP3)
+    # Select the type of audio file you want returned
+    audio_config = texttospeech.types.AudioConfig(
+        audio_encoding=texttospeech.enums.AudioEncoding.MP3)
 
-# Perform the text-to-speech request on the text input with the selected
-# voice parameters and audio file type
-response = client.synthesize_speech(synthesis_input, voice, audio_config)
+    # Perform the text-to-speech request on the text input with the selected
+    # voice parameters and audio file type
+    response = client.synthesize_speech(synthesis_input, voice, audio_config)
 
-# The response's audio_content is binary.
-with open('output.mp3', 'wb') as out:
-    # Write the response to the output file.
-    out.write(response.audio_content)
-    print('Audio content written to file "output.mp3"')
+    # The response's audio_content is binary.
+    with open('output.mp3', 'wb') as out:
+        # Write the response to the output file.
+        out.write(response.audio_content)
+        print('Audio content written to file "output.mp3"')
 
-pygame.mixer.init()
-pygame.mixer.music.load("output.mp3")
-pygame.mixer.music.play()
+    pygame.mixer.init()
+    pygame.mixer.music.load("output.mp3")
+    pygame.mixer.music.play()
 
-while pygame.mixer.music.get_busy():
-    pass
+    while pygame.mixer.music.get_busy():
+        pass
+
+if __name__ == "__main__":
+    main("This is a test. If you're hearing this, it worked.")
